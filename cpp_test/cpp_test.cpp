21c5746375e9383200b4097c6294d713353c47da
Flavio Ostap <pervaya.vstre4nya@gmail.com>
2020-08-01 19:43:56 +0300

    My first modified commit

--- cpp_test/cpp_test.cpp
+++ cpp_test/cpp_test.cpp
@@ -3,7 +3,17 @@
 using namespace std;
 
 int main(){
-    cout<<"My first commit!\n";
+    int i,j;
+    int another_total = 0;
+    int total = 0;
 
-    return 0;
+    for(i = 0;i < 10;i++){
+        total += i;
+    }
+
+    for(j = 0;j < 100;j++){
+        another_total += j;
+    } 
+
+    return total + another_total;
 }
